23224f80a58f22933c7cccd86232a99373cb90d9
Miguel Ramirez <xbizzybone@gmail.com>
2024-04-30 19:03:35 -0400

    set routes

--- app/index.js
+++ app/index.js
@@ -1,23 +1,23 @@
 router.get('/', (req, res) => {
-    res.redirect('/pages/login.html');
+    res.redirect('../pages/login.html');
 });
 
 // Path: /pages/login.html
 router.get('/pages/login.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/login.html'));
+    res.sendFile(path.join(__dirname, '../pages/login.html'));
 });
 
 // Path: /pages/userRegistration.html
 router.get('/pages/userRegistration.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/userRegistration.html'));
+    res.sendFile(path.join(__dirname, '../pages/userRegistration.html'));
 });
 
 // Path: /pages/recuperarContrasena.html
 router.get('/pages/recuperarContrasena.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/recuperarContrasena.html'));
+    res.sendFile(path.join(__dirname, '../pages/recuperarContrasena.html'));
 });
 
 // Path: /pages/dashboard.html
 router.get('/pages/dashboard.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/dashboard.html'));
+    res.sendFile(path.join(__dirname, '../pages/dashboard.html'));
 });
