833a3e107b7e07a832507c9240be5ebcac45bf21
Miguel Ramirez <xbizzybone@gmail.com>
2024-04-30 19:06:24 -0400

    test router

--- app/index.js
+++ app/index.js
@@ -1,23 +1,14 @@
+module.exports = (app) => {
+    app.use('/', require('./routes'));
+}
+
+// Path: app/routes/index.js
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+module.exports = router;
+
 router.get('/', (req, res) => {
-    res.redirect('../pages/login.html');
-});
-
-// Path: /pages/login.html
-router.get('/pages/login.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../pages/login.html'));
-});
-
-// Path: /pages/userRegistration.html
-router.get('/pages/userRegistration.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '../pages/userRegistration.html'));
-});
-
-// Path: /pages/recuperarContrasena.html
-router.get('/pages/recuperarContrasena.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '../pages/recuperarContrasena.html'));
-});
-
-// Path: /pages/dashboard.html
-router.get('/pages/dashboard.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '../pages/dashboard.html'));
 });
